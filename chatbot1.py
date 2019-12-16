a2619ae4d345384dff6e82a3a5463d30824e9e18
BEKIR MUGAYITOGLU <bmugayitoglu@gmail.com>
2019-12-15 21:09:11 -0800

    Update chatbot1.py

--- chatbot1.py
+++ chatbot1.py
@@ -1,4 +1,4 @@
 print("Hello. I am helper. I am a chatbot")
 print("I like playing soccer and I love to talk about about computer science education.")
 name = input("What is your name?: ")
-print("Hello", name, ", Nice to meet you")
+print("Hello", name, ", Nice to meet you!")
